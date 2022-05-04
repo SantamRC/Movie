e12a078d2890e3072387e14a2c347e78c12edce1
SantamRC <santamdev404@gmail.com>
2022-05-04 16:56:25 +0530

    Added MovieDB

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { MovieDb } from 'moviedb-promise';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  public mdb = new MovieDb('4d51e2149ffec1e3fabb84a54d724b76');
+
+  constructor() {}
+}
